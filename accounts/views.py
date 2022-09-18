fb2d5ea6eb7aa1ac12c8f7d44c1011d8791f01a5
github_username <email_address>
2022-09-18 13:53:54 +0430

    git init

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
