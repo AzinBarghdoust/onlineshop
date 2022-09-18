79d85b17795e5181e43ae4e20ab0baa0a5eb561e
github_username <email_address>
2022-09-18 15:50:50 +0430

    git init

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'pages',
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,5 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
